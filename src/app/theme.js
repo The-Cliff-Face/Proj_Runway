a2a1a78a08b654f96be1098ead2179a5b2bef29e
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-06-20 16:00:02 -0400

    Import theme information from https://zenoo.github.io/mui-theme-creator/, make the signin and signup pages have purple background/white text

--- src/app/theme.js
+++ src/app/theme.js
@@ -12,6 +12,43 @@ const montserrat = Montserrat({
 });
 
 const theme = createTheme({
+    // colors ripped from https://zenoo.github.io/mui-theme-creator/
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#ffffff',
+        },
+        secondary: {
+            main: '#fc6399',
+        },
+        background: {
+            default: '#2a1233',
+            paper: '#2a1233',
+        },
+        error: {
+            main: '#f64747',
+        },
+        warning: {
+            main: '#f9bf3b',
+        },
+        info: {
+            main: '#009fd4',
+        },
+        success: {
+            main: '#00b16a',
+        },
+    },
+    // disable button ripple globally (see https://mui.com/material-ui/getting-started/faq/#how-can-i-disable-the-ripple-effect-globally)
+    components: {
+        // Name of the component
+        MuiButtonBase: {
+            defaultProps: {
+                // The props to apply
+                disableRipple: true, // No more ripple, on the whole application
+            },
+        },
+    },
+
     typography: {
         fontFamily: montserrat.style.fontFamily,
     },
