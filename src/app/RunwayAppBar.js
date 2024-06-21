d7085ce165e35e5cc88e076f1219d8d715cb5042
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-06-20 20:22:45 -0400

    Add link to MUI docs

--- src/app/RunwayAppBar.js
+++ src/app/RunwayAppBar.js
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+// https://mui.com/material-ui/react-app-bar/
 export default function RunwayAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
