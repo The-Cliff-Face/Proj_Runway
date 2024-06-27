2f42459bb85c3ae60cd9b8cf9f47f4d4d03b69e5
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-06-26 22:31:39 -0400

    Add placeholder paragraph on root page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
     <>
-      <Container maxWidth="sm">
+      <Box maxWidth="sm" sx={{ m: 'auto' }} display="flex">
         <img src="/RunwayLogo.png" />
-      </Container>
+      </Box>
+
+      <Box maxWidth="md" sx={{ m: 'auto' }}>
+        <Typography sx={{ mb: 2 }} variant="h2" align="inherit">What is Runway?</Typography>
+        <Typography paragraph align="inherit">
+          Elit duis tristique sollicitudin nibh sit amet commodo. Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus. Convallis posuere morbi leo urna molestie at. Quis risus sed vulputate odio ut enim. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Eget mi proin sed libero enim sed faucibus turpis in. Imperdiet nulla malesuada pellentesque elit eget. Eu non diam phasellus vestibulum lorem sed risus ultricies. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. In hendrerit gravida rutrum quisque non tellus orci ac.
+        </Typography>
+      </Box>
     </>
   );
 }
