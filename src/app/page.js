717479373f0e1914108cc22ecc067439fdd3f856
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-06-26 21:40:48 -0400

    Add Runway logo to root page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
 export default function Home() {
   return (
     <>
+      <Container maxWidth="sm">
+        <img src="/RunwayLogo.png" />
+      </Container>
     </>
   );
 }
