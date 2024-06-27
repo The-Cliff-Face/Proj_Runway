dfeb45fe9dec4bfe6365c50b2d8b33e6b238923b
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-06-26 21:08:35 -0400

    Change site navigation bar to match format described in group meeting

--- src/app/RunwayAppBar.js
+++ src/app/RunwayAppBar.js
@@ -6,6 +6,19 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Divider from '@mui/material/Divider';
+
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonIcon from '@mui/icons-material/Person';
+
+
+// The current app bar is designed for a logged in user.
+// We need to keep track of whether or not the user is signed in
+// somehow and conditionally render a different navbar if they are
+// not signed in, as they cannot access the gallery home page, profile page, or logout.
+// I think a good setup would be the home page redirects to the root (welcome) page,
+// my profile changes to login, and logout can change to signup.
 
 // https://mui.com/material-ui/react-app-bar/
 export default function RunwayAppBar() {
@@ -13,19 +26,9 @@ export default function RunwayAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Button sx={{ mr: 'auto' }} href="/home" startIcon={<HomeIcon />} size="large" color="inherit">Home</Button>
+          <Button href="/" startIcon={<PersonIcon />} size="large" color="inherit">My Profile</Button>
+          <Button href="/" startIcon={<LogoutIcon />} size="large" color="inherit">Log Out</Button>
         </Toolbar>
       </AppBar>
     </Box>
