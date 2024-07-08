dc22a79ee186b257cc6cfabd1f8b24a4da1a6d1c
Jacob Plotz <jacobplotz@knights.ucf.edu>
2024-07-08 14:24:05 -0400

    create sendVerificationEmail()

--- mailgun.js
+++ mailgun.js
@@ -3,14 +3,25 @@ require('dotenv').config()
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY});
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
-mg.messages.create('mg.projectrunway.tech', {
-    from: 'signup@mg.projectrunway.tech',
-    to: ['ja347685@ucf.edu'],
-    subject: "test",
-    text: "Testing some Mailgun awesomeness!",
-    html: "<h1>Testing some Mailgun awesomeness!</h1>"
-})
-    .then(msg => console.log(msg)) // logs response data
-    .catch(err => console.log(err)); // logs any error
+function sendVerificationEmail(code, recipient) {
+    mg.messages.create('mg.projectrunway.tech', {
+        from: 'signup@mg.projectrunway.tech',
+        to: [recipient],
+        subject: "Please verify your Runway account",
+        text: `Your verification code is ${code}`,
+    })
+        .then(msg => console.log(msg)) // logs response data
+        .catch(err => console.log(err)); // logs any error
+}
+
+sendVerificationEmail(1234, 'ja347685@ucf.edu');
+
+
+// email the 4-digit code to the email provided upon signup
+// do it in line 111 of server.js
+// create an input box for the code and just wait until the user enters the code.
+// if the code is an error, just stay on that particular page
+// if the code is successful, then redirect to gallery page, 
+// set the user's verified status to true
