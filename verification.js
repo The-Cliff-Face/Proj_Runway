2bf7ee1bbabc3e4485f5ba5c5d38d92e1c7bdab9
mistrynick <84347368+mistrynick@users.noreply.github.com>
2024-07-13 16:00:36 -0400

    add api endpoints

--- verification.js
+++ verification.js
@@ -17,7 +17,32 @@ const validateUsername = (username) => {
     );
 }
 
+
+
+const validateRecommendationInput = (recommendationMatrix) => {
+  const colors = ["red", "blue", "black", "yellow", "pink", "white", "brown", "green", "purple", "orange", "gray", "teal"];
+  const clothes = ["trousers", "tshirts", "blouses", "dresses", "jeans", "shorts", "tank tops", "skirts", "hoodies", "sweatpants", "sweaters", "coats"];
+  if (!recommendationMatrix || !recommendationMatrix.clothes || !recommendationMatrix.colors) {
+    return false;
+  }
+  
+  for (let i = 0; i<colors.length; i++) {
+    if (!recommendationMatrix.colors.hasOwnProperty(colors[i])) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i<colors.length; i++) {
+    if (!recommendationMatrix.clothes.hasOwnProperty(clothes[i])) {
+      return false;
+    }
+  }
+  return true;
+
+}
+
 module.exports = {
     validateEmail,
-    validateUsername
+    validateUsername,
+    validateRecommendationInput,
 }
